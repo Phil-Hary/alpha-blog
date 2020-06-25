0301984e6cc5ca7b37f9dd9eecc0fe3fcc1e4846
Phil <project9237@gmail.com>
2020-06-25 21:35:09 +0530

    install gem BCrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	before_save { self.email = email.downcase }
 	has_many :articles
+	
 	validates :username, presence: true,
 											 length: { minimum: 3, maximum: 25 },
 											 uniqueness: { case_sensitive: false}
@@ -10,4 +11,7 @@ class User < ApplicationRecord
 	validates :email, presence: true,
 										length: { maximum: 105 },
 										format: { with: VALIDATE_EMAIL_REGEX }
+										
+	has_secure_password
+	
 end
