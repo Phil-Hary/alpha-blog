ff60f98842e1ae333faf6e4de4c2a113f832445e
Phil <project9237@gmail.com>
2020-06-26 13:48:15 +0530

    Add signup page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+	
+	def new
+		@user = User.new
+	end
+	
+	def create
+		@user = User.new(filter_params)
+		
+		if @user.save
+			flash[:notice] = "Welcome to Alpha Blog #{@user.username}!!!, you have been signed up successfully."
+			redirect_to articles_path
+		else
+			render 'new'
+		end
+		
+	end
+	
+	private
+	
+	def filter_params
+		params.require(:user).permit(:username, :email, :password)
+	end
+	
+end
